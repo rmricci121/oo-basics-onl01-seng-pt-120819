de5f3ebdf8658b053c28d37325b560086443b0b0
Robert M Ricci <rmricci121@gmail.com>
2019-12-28 01:56:40 +0000

    Automatically backed up by Learn

--- lib/shoe.rb
+++ lib/shoe.rb
@@ -8,11 +8,11 @@ class Shoe
   end
 
   def cobble
-    if condition == "old" 
+    if @condition == "old" 
     puts "Your shoe is as good as new!"
     end
     
-    condition = "new"
+    @condition = "new"
   end
 
 
