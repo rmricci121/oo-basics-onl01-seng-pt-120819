494ce79dae8e1814ad2fb34c083ea028b5a3051e
Robert M Ricci <rmricci121@gmail.com>
2019-12-28 01:41:06 +0000

    Automatically backed up by Learn

--- lib/shoe.rb
+++ lib/shoe.rb
@@ -1,13 +1,18 @@
 class Shoe
 
-attr_accessor :color, :size, :material, :condition
-attr_reader :brand
+  attr_accessor :color, :size, :material, :condition
+  attr_reader :brand
 
-def initialize(brand)
-  @brand = brand
-end
+  def initialize(brand)
+    @brand = brand
+  end
 
-
+  def cobble
+  if @condition == "new" 
+    puts "New"
+  else
+    puts "The shoe has been repaired"
+  end
 
 
 end
